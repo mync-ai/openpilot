095ee47f72576f861aceef565b68d248e0a38398
Enxu-Liu <enxu@msync.ai>
2025-06-23 17:26:18 -0400

    add another logger, see if boot normally

--- opendbc_repo/opendbc/car/carlog.py
+++ opendbc_repo/opendbc/car/carlog.py
@@ -10,3 +10,13 @@ carlog.propagate = False
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter('%(message)s'))
 carlog.addHandler(handler)
+
+# customize our logging
+telemetry_log = logging.getLogger('telemetry_log')
+logging.addLevelName(25, "TELEMETRY");telemetry_log.setLevel("TELEMETRY")
+telemetry_log.propagate = False
+
+# telemetry_handler = logging.handlers.SocketHandler('172.20.10.14', 9999)
+telemetry_handler = logging.FileHandler("/data/openpilot/telemetry.log", mode='a', encoding='utf-8', delay=True)
+telemetry_handler.setFormatter(logging.Formatter('%(message)s'))
+telemetry_log.addHandler(telemetry_handler)
