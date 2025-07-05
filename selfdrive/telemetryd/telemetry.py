32ca758cf67a45732b1b41fc12c544e97ab663b4
Enxu-Liu <enxu@msync.ai>
2025-07-05 18:41:44 -0400

    reconnect fix 2

--- selfdrive/telemetryd/telemetry.py
+++ selfdrive/telemetryd/telemetry.py
@@ -41,21 +41,21 @@ def main():
   #     tel.log(25, (str(curr_milli_time())+" test_"+str(tel_idx)+'\n'))
   #     tel_idx += 1
   #     time.sleep(1)
-
-  s = connect("172.20.10.14", 9999, retry_delay=2)
-  tel_idx = 0
-  try:
-    while True:
-      msg = str(curr_milli_time())+" test_"+str(tel_idx)
-      s.sendall(msg.encode('utf-8'))
-      tel_idx += 1
-      print(f"Sent: {tel_idx}")
+  while True:
+    s = connect("172.20.10.14", 9999, retry_delay=2)
+    tel_idx = 0
+    try:
+      while True:
+        msg = str(curr_milli_time())+" test_"+str(tel_idx)
+        s.sendall(msg.encode('utf-8'))
+        tel_idx += 1
+        print(f"Sent: {tel_idx}")
+        time.sleep(1)
+    except (BrokenPipeError, ConnectionResetError, OSError) as e:
+      # print(f"Connection lost: {e}")
+      s.close()
+      # print("Reconnecting...")
       time.sleep(1)
-  except (BrokenPipeError, ConnectionResetError, OSError) as e:
-    # print(f"Connection lost: {e}")
-    s.close()
-    # print("Reconnecting...")
-    time.sleep(1)
 
 if __name__ == "__main__":
     main()
