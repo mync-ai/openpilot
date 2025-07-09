57432a18d6002865b533651100c5bf84d3173a9c
Enxu-Liu <enxu@msync.ai>
2025-07-09 12:10:15 -0400

    multi-channel telemetry

--- selfdrive/telemetryd/telemetry.py
+++ selfdrive/telemetryd/telemetry.py
@@ -20,20 +20,34 @@ import cereal.messaging as messaging
 #   return telemetry_log
 
 def curr_time():
-    return time.time_ns() / 1e9
-    # return time.perf_counter()
+  return time.time_ns() / 1e9
+  # return time.perf_counter()
 
-def connect(ip, port, retry_delay=2):
+class channel:
+  def __init__(self, name:str, msg_type):
+    self.name = name
+    self.msg_type = msg_type
+    self.socket = self.create_socket()
+    # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) ?
+    # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) ?
+  
+  def create_socket(self):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    return s
+
+  def connect(self, ip, port, retry_delay=2):
     while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-            s.connect((ip, port))
-            # print(f"Connected to {ip}:{port}")
-            return s
-        except (OSError, socket.error) as e:
-            # print(f"Connection failed: {e}. Retrying in {retry_delay}s...")
-            time.sleep(retry_delay)
+      try:
+        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        self.socket.connect((ip, port))
+        # print(f"Connected to {ip}:{port}")
+        # return s
+        return
+      except (OSError, socket.error) as e:
+        # print(f"Connection failed: {e}. Retrying in {retry_delay}s...")
+        time.sleep(retry_delay)
 
 def main():
   # tel = setup_logging_telemetry()
@@ -42,19 +56,24 @@ def main():
   #     tel.log(25, (str(curr_milli_time())+" test_"+str(tel_idx)+'\n'))
   #     tel_idx += 1
   #     time.sleep(1)
+
+  
+  TCP1_channel = channel("TCP1", "tcp1")
+  TCP2_channel = channel("TCP2", "tcp2")
+
   while True:
-    s = connect("172.20.10.14", 9999, retry_delay=2)
+    TCP1_channel.connect("172.20.10.14", 9999, retry_delay=2)
     tel_idx = 0
     try:
       while True:
         msg = str(curr_time())+" test_"+str(tel_idx)
-        s.sendall(msg.encode('utf-8'))
+        TCP1_channel.socket.sendall(msg.encode('utf-8'))
         tel_idx += 1
         print(f"Sent: {tel_idx}")
         time.sleep(1)
     except (BrokenPipeError, ConnectionResetError, OSError) as e:
       # print(f"Connection lost: {e}")
-      s.close()
+      TCP1_channel.socket.close()
       # print("Reconnecting...")
       time.sleep(1)
 
