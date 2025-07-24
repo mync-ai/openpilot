a864f5d7b73258b393b0a857c9574956c7b2f042
Enxu-Liu <enxu@msync.ai>
2025-07-24 21:28:31 +0000

    adding telemetry feature to sunny pilot branch

---
+++ selfdrive/telemetryd/telemetry.py
@@ -0,0 +1,92 @@
+import os
+import logging
+import logging.handlers
+import socket
+
+import time
+from openpilot.selfdrive.telemetryd.subscriber import *
+
+# def setup_logging_telemetry():
+#   # customize our logging
+#   telemetry_log = logging.getLogger('telemetry_log')
+#   logging.addLevelName(25, 'TELEMETRY');telemetry_log.setLevel('TELEMETRY')
+#   telemetry_log.propagate = False
+
+  # telemetry_handler = logging.handlers.SocketHandler('172.20.10.14', 9999)
+#   # telemetry_handler = logging.FileHandler("/data/openpilot/telemetry.log", mode='w', encoding='utf-8', delay=False)
+#   telemetry_handler.setFormatter(logging.Formatter('%(message)s'))
+#   telemetry_log.addHandler(telemetry_handler)
+#   # telemetry_log.log(25, "test telemetry message!\n")
+#   return telemetry_log
+
+def curr_time():
+  return time.time_ns() / 1e9
+  # return time.perf_counter()
+
+class channel:
+  def __init__(self, name:str, msg_type):
+    self.name = name
+    self.msg_type = msg_type
+    self.socket = self.create_socket()
+    # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) ?
+    # self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1) ?
+  
+  def create_socket(self):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    return s
+
+  def connect(self, ip, port, retry_delay=2):
+    while True:
+      try:
+        # s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        print(f"Connecting to {ip}:{port}...")
+        self.socket.connect((ip, port))
+        # print(f"Connected to {ip}:{port}")
+        # return s
+        return
+      except (OSError, socket.error) as e:
+        print(f"Connection failed: {e}. Retrying in {retry_delay}s...")
+        time.sleep(retry_delay)
+
+def main():
+  # tel = setup_logging_telemetry()
+  # tel_idx = 0
+  # while True:
+  #     tel.log(25, (str(curr_milli_time())+" test_"+str(tel_idx)+'\n'))
+  #     tel_idx += 1
+  #     time.sleep(1)
+  
+  TCP1_channel = channel("TCP1", "tcp1")
+  TCP2_channel = channel("TCP2", "tcp2")
+
+  while True:
+    TCP1_channel.connect("192.168.1.111", 9999, retry_delay=2)
+    # TCP2_channel.connect("192.168.1.110", 9998, retry_delay=2)
+    tel_idx = 0
+    try:
+      while True:
+        acc,jerks = get_cereal_data()
+        print(f"Received: {acc}, {jerks}")
+        if acc and jerks:
+          msg = f" Acc: {acc}, Jerks: {jerks}"
+        else:
+          msg = " Invalid data"
+        msg = str(curr_time())+msg
+
+        # msg = str(curr_time())+" test_"+str(tel_idx)
+
+        TCP1_channel.socket.sendall(msg.encode('utf-8'))
+        tel_idx += 1
+        print(f"Sent: {tel_idx}")
+        time.sleep(0.1)
+    except (BrokenPipeError, ConnectionResetError, OSError) as e:
+      print(f"Connection lost: {e}")
+      TCP1_channel.socket.close()
+      TCP1_channel.socket = TCP1_channel.create_socket()
+      print("Reconnecting...")
+      time.sleep(1)
+
+if __name__ == "__main__":
+    main()
