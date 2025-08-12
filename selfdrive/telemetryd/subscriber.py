794c022f6f5d26f546068f7584001267a79b6da3
Bradley Kerr <kerrb@msync.ai>
2025-08-12 13:51:57 -0400

    Reverted subscriber.py to nested if's

--- selfdrive/telemetryd/subscriber.py
+++ selfdrive/telemetryd/subscriber.py
@@ -100,25 +100,26 @@ def get_cereal_data(interesting_subs=['longitudinalPlan', 'modelV2'], verbose=Fa
     return listen_plan(sm['longitudinalPlan'])
 
 def get_short_control():
+    cmd_out = "neutral"
     sm.update()
     if sm.updated['seatControl']:
-        cmd_map = {
-            0: "neutral",
-            1: "forward",
-            2: "back",
-            3: "left",
-            4: "right",
-            5: "left",
-            6: "right"
-        }
-        print(sm['seatControl'].command)
         cmd_val = sm['seatControl'].command
-        return cmd_map.get(cmd_val, "neutral")
+        if cmd_val == "mildLeft" or cmd_val == "hardLeft":
+            cmd_out = "left"
+        elif cmd_val == "mildRight" or cmd_val == "hardRight":
+            cmd_out = "right"
+        elif cmd_val == "neutral":
+            cmd_out = "neutral"
+        elif cmd_val == "forward":
+            cmd_out = "forward"
+        elif cmd_val == "back":
+            cmd_out = "back"
+        return cmd_out
 
 # if __name__ == "__main__":
 #     latency = 0.1  # seconds, change as needed
 #     interesting_subs = ['longitudinalPlan', 'modelV2']
-#     parse_messages(interesting_subs, latency)
+#     parse_messages(interesting_subs, latency, verbose=True)
 
 
 # Model Outputs:
